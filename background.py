07a45b0e9d0820ce8423706c13b706ae956215d1
Shoofle <xsigma@gmail.com>
2013-04-09 02:57:44 -0400

    Perlin noise, and noise in general. Is this the start of something bigger? Maybe\!

--- background.py
+++ background.py
@@ -1,10 +1,11 @@
 import pyglet
 import random
+from random import uniform
 import math
 from vectors import v
 
 class Background(object):
-	def __init__(self, pscreen, num_triangles=5000, x=(-8000,8000), y=(-5000,5000), z=(-100,100), width=200, height=0.2):
+	def __init__(self, pscreen, num_triangles=5000, x=(-8000,8000), y=(-5000,5000), z=(-100,100), width=200):
 		self.pscreen = pscreen
 		self.dead = False
 		self.vlist = pyglet.graphics.vertex_list(3*num_triangles, 'v3f', 'c3f')
@@ -12,13 +13,11 @@ class Background(object):
 		verts_per_triangle = 3
 		for triangle_index in range(num_triangles):
 			color = random.random(), random.random(), random.random()
-			cx, cy, cz = random.uniform(x[0],x[1]), random.uniform(y[0],y[1]), random.uniform(z[0],z[1])
-			while -0.2 < cz < 0.2: cz = random.uniform(z[0],z[1])
+			cx, cy, cz = uniform(x[0],x[1]), uniform(y[0],y[1]), uniform(z[0],z[1])
 			for sub_index in range(3):
+				vert = cx+uniform(-width/2.0, width/2.0), cy + uniform(-width/2.0, width/2.0), cz + uniform(-width/2.0, width/2.0)
 				vertex_index = (triangle_index*verts_per_triangle + sub_index)*3
-				self.vlist.vertices[vertex_index:vertex_index+3] = (cx + random.uniform(-width/2,width/2)), \
-																   (cy + random.uniform(-width/2,width/2)), \
-																   (cz + random.uniform(-height/2,height/2))
+				self.vlist.vertices[vertex_index:vertex_index+3] = vert
 				self.vlist.colors[vertex_index:vertex_index+3] = color
 
 		x_count, y_count = 200, 200
@@ -36,10 +35,6 @@ class Background(object):
 			for i in range(x_count):
 				x_n, y_n = 1.0*i/x_count, 1.0*j/y_count
 				p = sum(map(lambda c: perlin_2d(x_n,y_n, source=rands[c[0]])*c[1],coefficients) )
-				#p = (perlin_2d(x_n,y_n, source=r2) + perlin_2d(x_n,y_n, source=r_v1) + perlin_2d(x_n,y_n, source=r_v2) + perlin_2d(x_n,y_n, source=r_v3))*0.25
-#				p = perlin_2d(x_n, y_n, source=r_2d)
-#				p = perlin_2d(x_n, y_n, source=r_downscaled)
-#				p = perlin_2d(x_n, y_n, source=r_doubledown)
 
 				xp, yp, zp = x_width*x_n+start_x, y_width*y_n+start_y, offset_z+depth*p
 				r, theta, h = cylinder_radius + depth*p, 2*math.pi*x_n, cylinder_height*y_n
@@ -80,9 +75,9 @@ class Background(object):
 		pyglet.gl.glPushMatrix()
 		self.vlist.draw(pyglet.gl.GL_TRIANGLES)
 		#pyglet.gl.glRotatef(self.pscreen.total_time*20, 0.0, 1.0, 0.0)
-		pyglet.gl.glMaterialfv(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_DIFFUSE, (pyglet.gl.GLfloat * 4)(*(1, 1, 1, 1.0)))
-		pyglet.gl.glMaterialfv(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_SPECULAR, (pyglet.gl.GLfloat * 4)(*(0.9, 0.9, 0.8, 1.0)))
-		pyglet.gl.glMaterialf(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_SHININESS, 89)
+		#pyglet.gl.glMaterialfv(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_DIFFUSE, (pyglet.gl.GLfloat * 4)(*(1, 1, 1, 1.0)))
+		#pyglet.gl.glMaterialfv(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_SPECULAR, (pyglet.gl.GLfloat * 4)(*(0.9, 0.9, 0.8, 1.0)))
+		#pyglet.gl.glMaterialf(pyglet.gl.GL_FRONT_AND_BACK, pyglet.gl.GL_SHININESS, 89)
 		self.vlist2.draw(pyglet.gl.GL_TRIANGLES)
 		pyglet.gl.glPopMatrix()
 
@@ -126,19 +121,8 @@ def hermite(p, c):
 	""" Hermite interpolation between two points, with starting and ending tangents zero. """
 	return c[0]*(2*(p**3) - 3*(p**2) + 1) + c[1]*(-2*(p**3) + 3*(p**2))
 
-#def spectral_noise(x, y, f_r = (0,0.01), v=r):
-#	output = 0
-#	for xf, xo, yf, yo in zip(*v):
-#		xf, yf = xf*(f_r[1]-f_r[0])+f_r[0], yf*(f_r[1]-f_r[0])+f_r[0]
-#		output += math.sin(xf*(x-xo)) * math.sin(yf*(y-yo))
-#	return output/len(r1)
-
-
-#r = [[random.random() for i in range(100)] for j in range(5)]
-
-#r2 = [[v(random.uniform(-1,1),random.uniform(-1,1)) for i in range(80)] for j in range(80)]
 rands = list()
-rands.append( [[v(random.uniform(-1,1),random.uniform(-1,1)) for i in range(100)] for j in range(100)] )
+rands.append( [[v(uniform(-1,1),uniform(-1,1)) for i in range(100)] for j in range(100)] )
 for i in range(10):
 	if len(rands[i]) < 10: break
 	rands.append(rescale_2d(rands[i], 0.5))
